6c2dcecfc81651d12bf2c15f56c064584329ac9b
Zhenua <zhenua.gladkovv@gmail.com>
2021-04-07 14:37:53 +0300

    imp(gulp) add index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 
-const { src, dest, watch, parallel, series, task } = require ('gulp');
+const { src, dest, watch, parallel, series } = require ('gulp');
 
-const gulp        = require('gulp');
+
 const scss         = require('gulp-sass');
 const concat       = require('gulp-concat');
 const browserSync  = require('browser-sync').create();
@@ -12,48 +12,15 @@ const del          = require('del');
 
 const pug = require('gulp-pug');
 
-// function add() {
-//     task('pug', function buildHTML() {
-//         return src('app/Pug/*.pug')
-//         .pipe(pug({
-//             pretty:true
-//         }))
-//         .pipe(dest('app'));
-//       });
-// }
 
-//   task('pug', function buildHTML() {
-//   return src('app/Pug/*.pug')
-//   .pipe(pug({
-//       pretty:true
-//   }))
-//   .pipe(dest('app'));
-// });
-const paths = {
-    src: 'src',
-    dist: 'dist'
-  };
-
-const pugFiles = {
-    src: paths.src + 'app/Pug/*.pug',
-    dist: paths.dist,
-    watch: paths.src + 'app/Pug/*.pug'
-  }
-  
-  gulp.task('pug', function() {
-    gulp.src(pugFiles.src)
-      .pipe(pug({
-        locals: {},
-        pretty: true
-      }))
-      .pipe(gulp.dest(pugFiles.dist))
-  });
-  function reloadBrowserSyncPug() {
-    setTimeout(reloadBrowserSync, 500);
-  }
-  
-
-
+function pugHtml () {
+    return src('app/Pug/*.pug')
+        .pipe(pug({
+            locals: {},
+            pretty: true
+        }))
+        .pipe(dest('app'))
+}
 
 function browsersync() {
     browserSync.init({
@@ -87,10 +54,6 @@ function images() {
 
 function scripts() {
     return src([
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -109,7 +72,6 @@ function styles(){    //styles-просто имя(название) функц�
         }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
-
 }
 function build () {
     return src([
@@ -121,27 +83,17 @@ function build () {
     ], {base:'app'})
     .pipe(dest('dist'))
 }
-
-
-
 function watching(){
     watch(['app/scss/**/*.scss'], styles );
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts ); // ! - этот знак означает кроме меня
     watch(['app/*.html']).on('change', browserSync.reload);
-    // task('watch', function () {
-    //     return watch('app/Pug/*.pug', { ignoreInitial: false })
-    //     .pipe(dest('app'));
-    //     });
 
-
-        //  watch(['app/Pug/*.pug'], add );
-    task('watch', [], function() {
-        watch(pugFiles.watch, ['pug', reloadBrowserSyncPug]);
-    })
+    watch(['app/Pug/*pug'], pugHtml);
 
 }
 
 exports.styles = styles;
+exports.pugHtml = pugHtml;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.scripts = scripts;
@@ -149,8 +101,5 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 
 
-
-
-
 exports.build = series(cleanDist, images, build );
-exports.default = parallel(styles ,browsersync, watching, scripts);
+exports.default = parallel(styles ,browsersync, watching, scripts,pugHtml);
